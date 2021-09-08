e1fee3c1ee569a8063e248e0adea3d660436dddf
Rohaan Malik <36078297+rohaanmalik@users.noreply.github.com>
2021-09-08 23:50:15 +0000

    bug fixed with send 404 error

--- pages/api/twitch.js
+++ pages/api/twitch.js
@@ -2,13 +2,39 @@
 const HOST_NAME = "https://api.twitch.tv/helix"
 
 
+
+export default async (req, res) => {
+  try {
+    if (req.method === 'POST'){
+      const { data } = req.body
+
+      const channelData = await getTwitchChannel(data);
+
+      if (channelData){
+        console.log("Channel Data: ",channelData)
+        res.status(200).json({ data: channelData})
+      } else {
+        res.status(404).json({ data })
+      }
+
+    }
+  } catch (error) {
+    res.status(500).json({data: 'Welcome to Twitch API!'})
+  }
+}
+
+
+
 const getTwitchChannel = async channelName => {
   console.log("Searching for twitch channel")
+
   if (channelName) {
+
     const accessToken = await getTwitchAccessToken();
+
     if (accessToken){
 
-      const response = await fetch(`${HOST_NAME}/search/channels?query=${channelName}`, {
+      const response = await fetch(`${HOST_NAME}/search/channels?query=${channelName}`,{
         headers: {
           Authorization: `Bearer ${accessToken}`,
           "Client-Id": process.env.TWITCH_CLIENT_ID
@@ -16,9 +42,10 @@ const getTwitchChannel = async channelName => {
       })
 
       const json = await response.json()
+
       if (json.data) {
         const { data } = json;
-
+        console.log("REACHED HEREEEEEEEEEEEE")
         const lowercaseChannelName = channelName.toLowerCase()
 
         const foundChannel = data.find(
@@ -26,17 +53,16 @@ const getTwitchChannel = async channelName => {
             const lowerCaseDisplayName = channel.display_name.toLowerCase()
 
             return lowercaseChannelName === lowerCaseDisplayName
-          }
-
+          })
+          // console.log(" FOUND CHANNEL ", foundChannel)
           return foundChannel;
-        )
       }
     }
 
-    throw new Error(" Twitch accessToken was undefined")
+    throw new Error("Twitch accessToken was undefined")
   }
 
-  throw new Error(" No channel name provided")
+  throw new Error("No channel name provided")
 }
 
 const getTwitchAccessToken = async () => {
@@ -53,14 +79,4 @@ const getTwitchAccessToken = async () => {
 }
 
 
-export default async (req, res) => {
-  try {
-    if (req.method === 'POST'){
-      const { data } = req.body
-      res.status(200).json({ data })
-    }
-  } catch (error) {
-    res.status(200).json({data: 'Welcome to Twitch API!'})
-  }
-}
 
